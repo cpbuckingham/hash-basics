2b60679404dd94e45254f685a46c4d16748dce03
Cameron <cameron.p.buckingham@gmail.com>
2014-06-19 13:29:56 -0600

    01

--- 01_last_name.rb
+++ 01_last_name.rb
@@ -1,3 +1,12 @@
 require_relative "person"
 
-# Print Bjorn's last name
+bjorn_borg = {
+  :first_name => "Bjorn",
+  :last_name => "Borg",
+  :wears_glasses => true,
+  :address => {"street" =>"444 Borg lane", "city" => "San Francisco", "state" => "CA", "zip code" => "94104"},
+  :favorite_foods => ["sushi", "hamburgers", "mexican food"],
+  :favorite_sport => "tennis"
+}
+
+puts bjorn_borg[:last_name]
