32f57c29c361040a381c2a55e25e4cd60e4fe7af
Cameron <cameron.p.buckingham@gmail.com>
2014-06-19 13:32:28 -0600

    02

--- 02_favorite_sport.rb
+++ 02_favorite_sport.rb
@@ -1,3 +1,12 @@
 require_relative "person"
 
-# Print Bjorn's favorite sport
+bjorn_borg = {
+  :first_name => "Bjorn",
+  :last_name => "Borg",
+  :wears_glasses => true,
+  :address => {"street" =>"444 Borg lane", "city" => "San Francisco", "state" => "CA", "zip code" => "94104"},
+  :favorite_foods => ["sushi", "hamburgers", "mexican food"],
+  :favorite_sport => "tennis"
+}
+
+puts bjorn_borg[:favorite_sport]
