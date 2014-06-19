25dd6d24cc28f96ff6dbc7468b83cc26043705f9
Cameron <cameron.p.buckingham@gmail.com>
2014-06-19 13:58:09 -0600

    05

--- 05_print_address.rb
+++ 05_print_address.rb
@@ -1,3 +1,12 @@
 require_relative "person"
 
-# Print Bjorn's address. It should read "444 Borg Lane, San Francisco, CA, 94104"
+bjorn_borg = {
+  :first_name => "Bjorn",
+  :last_name => "Borg",
+  :wears_glasses => true,
+  :address => {"street" =>"444 Borg lane", "city" => "San Francisco", "state" => "CA", "zip code" => "94104"},
+  :favorite_foods => ["sushi", "hamburgers", "mexican food"],
+  :favorite_sport => "tennis"
+}
+
+puts "Bjorn Borg lives on #{bjorn_borg[:address][:street]}, #{bjorn_borg[:address][:city]}, #{bjorn_borg[:address][:state]}, #{bjorn_borg[:address][:zip_code]}"
