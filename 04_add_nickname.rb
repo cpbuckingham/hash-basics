f158fd7e7e0126fb80aa57df0532728d7a1079c9
Cameron <cameron.p.buckingham@gmail.com>
2014-06-19 13:44:20 -0600

    04

--- 04_add_nickname.rb
+++ 04_add_nickname.rb
@@ -1,3 +1,14 @@
 require_relative "person"
 
-# Add a nickname to the BJORN_BORG hash and print it out. Don't edit the person.rb file!
+bjorn_borg = {
+  :first_name => "Bjorn",
+  :last_name => "Borg",
+  :wears_glasses => true,
+  :nickname => "Iceland Queen",
+  :address => {"street" =>"444 Borg lane", "city" => "San Francisco", "state" => "CA", "zip code" => "94104"},
+  :favorite_foods => ["sushi", "hamburgers", "mexican food"],
+  :favorite_sport => "tennis"
+}
+
+puts "Bjorn, do you have a nickname"
+puts "Why yes I do! It's #{bjorn_borg[:nickname]}"
